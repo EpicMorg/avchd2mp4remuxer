4fe15af5adc4e1a187b84c80b08b25b7a1760412
STAM <stam@epicm.org>
2017-02-19 23:28:20 +0300

    Signed-off-by: STAM <stam@epicm.org>

---
+++ avchd2mp4remuxergui/FrmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace avchd2mp4remuxergui
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxDeps = new System.Windows.Forms.GroupBox();
+            this.chkDepTsMuxer = new System.Windows.Forms.CheckBox();
+            this.chkDepMp4Box = new System.Windows.Forms.CheckBox();
+            this.chkDepFFmpeg = new System.Windows.Forms.CheckBox();
+            this.gbxDeps.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxDeps
+            // 
+            this.gbxDeps.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbxDeps.Controls.Add(this.chkDepTsMuxer);
+            this.gbxDeps.Controls.Add(this.chkDepMp4Box);
+            this.gbxDeps.Controls.Add(this.chkDepFFmpeg);
+            this.gbxDeps.Location = new System.Drawing.Point(12, 12);
+            this.gbxDeps.Name = "gbxDeps";
+            this.gbxDeps.Size = new System.Drawing.Size(390, 44);
+            this.gbxDeps.TabIndex = 0;
+            this.gbxDeps.TabStop = false;
+            this.gbxDeps.Text = "Зависимости";
+            // 
+            // chkDepTsMuxer
+            // 
+            this.chkDepTsMuxer.AutoSize = true;
+            this.chkDepTsMuxer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chkDepTsMuxer.Location = new System.Drawing.Point(202, 19);
+            this.chkDepTsMuxer.Name = "chkDepTsMuxer";
+            this.chkDepTsMuxer.Size = new System.Drawing.Size(99, 17);
+            this.chkDepTsMuxer.TabIndex = 0;
+            this.chkDepTsMuxer.Text = "tsMuxeR.exe";
+            this.chkDepTsMuxer.UseVisualStyleBackColor = true;
+            // 
+            // chkDepMp4Box
+            // 
+            this.chkDepMp4Box.AutoSize = true;
+            this.chkDepMp4Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chkDepMp4Box.Location = new System.Drawing.Point(100, 19);
+            this.chkDepMp4Box.Name = "chkDepMp4Box";
+            this.chkDepMp4Box.Size = new System.Drawing.Size(96, 17);
+            this.chkDepMp4Box.TabIndex = 0;
+            this.chkDepMp4Box.Text = "MP4Box.exe";
+            this.chkDepMp4Box.UseVisualStyleBackColor = true;
+            // 
+            // chkDepFFmpeg
+            // 
+            this.chkDepFFmpeg.AutoSize = true;
+            this.chkDepFFmpeg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chkDepFFmpeg.Location = new System.Drawing.Point(6, 19);
+            this.chkDepFFmpeg.Name = "chkDepFFmpeg";
+            this.chkDepFFmpeg.Size = new System.Drawing.Size(88, 17);
+            this.chkDepFFmpeg.TabIndex = 0;
+            this.chkDepFFmpeg.Text = "ffmpeg.exe";
+            this.chkDepFFmpeg.UseVisualStyleBackColor = true;
+            this.chkDepFFmpeg.CheckedChanged += new System.EventHandler(this.chkDepFFmpeg_CheckedChanged);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 232);
+            this.Controls.Add(this.gbxDeps);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AvcHD2Mp4 remuxer GUI";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.gbxDeps.ResumeLayout(false);
+            this.gbxDeps.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxDeps;
+        private System.Windows.Forms.CheckBox chkDepTsMuxer;
+        private System.Windows.Forms.CheckBox chkDepMp4Box;
+        private System.Windows.Forms.CheckBox chkDepFFmpeg;
+    }
+}
+
---
+++ avchd2mp4remuxergui/FrmMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace avchd2mp4remuxergui
+{
+    public partial class FrmMain : Form
+    {
+        string pathToTools = Path.Combine(Application.StartupPath, "ffmpeg.exe");
+        string pathToTsMuxer = Path.Combine(Application.StartupPath, "tsMuxeR.exe");
+        string pathToFMp4Box = Path.Combine(Application.StartupPath, "MP4Box.exe");
+        public FrmMain()
+        {
+            InitializeComponent(); 
+        }
+        private void CheckDeps()
+        {
+            if (File.Exists(pathToTools))
+            {
+                chkDepFFmpeg.Checked = true;
+                chkDepFFmpeg.ForeColor = Color.GreenYellow;
+            }
+            else { 
+                chkDepFFmpeg.Checked = false;
+                chkDepFFmpeg.ForeColor = Color.Red;
+            }
+            if (File.Exists(pathToTsMuxer))
+            {
+
+            }
+            if (File.Exists(pathToFMp4Box))
+            {
+
+            }
+
+
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            CheckDeps();
+        }
+
+        private void chkDepFFmpeg_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckDeps();
+        }
+    }
+}
